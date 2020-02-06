d746d1c6c94333c399540aaa5dd8a6c3ed7a06bf
leo725 <liaozy725@163.com>
2020-02-06 22:31:46 +0800

    2020

--- public/static/config.js
+++ public/static/config.js
@@ -1,4 +1,4 @@
 window.g = {
-  baseURL: 'http://jingcaiagent.manggeek.com', // 竞猜代理接口地址
+  baseURL: 'http://agent.jkgame.club', // 竞猜代理接口地址
   guessH5Link: 'http://jiucaiapi.manggeek.com/layout/', // 竞猜用户端项目地址
 }
